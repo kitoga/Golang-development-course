a8f46b9f4286925a6668793d5fd50cfd01198b90
kitoga <jken04680@gmail.com>
2022-06-26 23:25:49 +0100

    sunday maps

--- demo/maps/maps.go
+++ demo/maps/maps.go
@@ -3,4 +3,32 @@ package main
 import "fmt"
 
 func main() {
+
+	shoppingList := make(map[string]int)
+	shoppingList["eggs"] = 7
+	shoppingList["milk"] = 2
+	shoppingList["bread"] = 2
+
+	shoppingList["eggs"] += 1
+	fmt.Println(shoppingList)
+
+	delete(shoppingList, "milk")
+	fmt.Println("Milk deleted from shoppingList", shoppingList)
+
+	fmt.Println("need", shoppingList["eggs"], "eggs")
+
+	coco, found := shoppingList["coco"]
+	fmt.Println("need coco?", coco)
+	if !found {
+		fmt.Println("coco not found!")
+		return
+	} else {
+		fmt.Println("coco is", shoppingList["coco"], "on th shopping List")
+	}
+
+	// totalItem := 0
+	// for _, amount := range shoppingList {
+	// 	totalItem = totalItem + amount
+	// }
+	fmt.Println("total length", len(shoppingList))
 }
--- exercise/maps/maps.go
+++ exercise/maps/maps.go
@@ -20,7 +20,9 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const (
 	Online      = 0
@@ -31,4 +33,33 @@ const (
 
 func main() {
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
+
+	//--Requirements:
+	//* Create a function to print server status displaying:
+	//  - number of servers
+	totalItem := ""
+	for _, amount := range servers {
+		totalItem = totalItem + amount
+	}
+	fmt.Println("total length", len(servers))
+	//  - number of servers for each status (Online, Offline, Maintenance, Retired)
+	//* Create a map using the server names as the key and the server status
+	//  as the value
+
+	serverStatus := []int{Online, Offline, Maintenance, Retired}
+
+	mapServers := map[string]int{}
+
+	mapServers["darkstar"] = serverStatus[Online]
+	fmt.Println(mapServers["darkstar"])
+
+	//* Set all of the server statuses to `Online` when creating the map
+	//* After creating the map, perform the following actions:
+	//  - call display server info function
+	//  - change server status of `darkstar` to `Retired`
+	//  - change server status of `aiur` to `Offline`
+	//  - call display server info function
+	//  - change server status of all servers to `Maintenance`
+	//  - call display server info function
+
 }
